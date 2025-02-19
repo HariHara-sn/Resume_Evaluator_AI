4e0b5b6fa05fa5869e055440a164bb99af085a82
HariHara-sn <sudhanabirami007@gmail.com>
2025-02-19 16:23:10 +0530

    combined-2inone

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 import streamlit as st
-from modules.users import process_user_mode
-from modules.recruiters import process_recruiters_mode
-from modules.admin import process_admin_mode
-from modules.feedback import process_feedback_mode
+from user_side import process_user_mode
+from recruiters import process_recruiters_mode
+
 
 def main():
-    st.set_page_config(page_title="Resume Parser", page_icon="✅")
+    st.set_page_config(page_title="Resume Evaluator", page_icon="✅")
 
     st.sidebar.title("Navigation")
-    app_mode = st.sidebar.selectbox("Choose an option", ["Users", "Recruiters", "Feedback", "Admin"])
+    app_mode = st.sidebar.selectbox("Choose an option", ["Users", "Recruiters"])
 
     if app_mode == "Users":
         process_user_mode()
 
-    elif app_mode == "Recruiters":
+    if app_mode == "Recruiters":
         process_recruiters_mode()
-
-    elif app_mode == "Admin":
-        process_admin_mode()
-
-    elif app_mode == "Feedback":
-        process_feedback_mode()
 
 if __name__ == "__main__":
     main()
